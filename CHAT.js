0f367c56139604c5ece815d4836acdc69c851f82
nato-git <s.f.h.o1219@gmail.com>
2025-06-14 00:17:55 +0900

    Add files via upload

---
+++ CHAT.js
@@ -0,0 +1,8 @@
+function SAVE(){
+    localStorage.setItem="name",value;
+    localStorage.setItem="comment",value;
+    var names = localStorage.getItem("name")
+    var comments = localStorage.getItem("comment");
+    var texts = "[" + names + "]" + "comments"
+    localStorage.setItem="texts",value;
+}
