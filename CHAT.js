648e7798b268b53f1498a23f792c7a712785a898
nato-git <s.f.h.o1219@gmail.com>
2025-06-14 00:21:39 +0900

    Update CHAT.js

--- CHAT.js
+++ CHAT.js
@@ -2,6 +2,10 @@ function SAVE(){
     localStorage.setItem="name",value;
     localStorage.setItem="comment",value;
     localStorage.setItem="se1",value;
+    localStorage.setItem="se2",value;
+    localStorage.setItem="se3",value;
+    localStorage.setItem="se4",value;
+    localStorage.setItem="se5",value;
     var names = localStorage.getItem("name")
     var comments = localStorage.getItem("comment");
     var texts = "[" + names + "]" + "comments"
