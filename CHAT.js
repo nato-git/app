0d0593719350ab9668442f2874b29415de98004f
nato-git <s.f.h.o1219@gmail.com>
2025-06-14 00:24:14 +0900

    Update CHAT.js

--- CHAT.js
+++ CHAT.js
@@ -8,6 +8,11 @@ function SAVE(){
     localStorage.setItem="se5",value;
     var names = localStorage.getItem("name")
     var comments = localStorage.getItem("comment");
-    var texts = "[" + names + "]" + "comments"
+    var texts ="[" + names + "]" + comments
+    var se1 = localStorage.getItem("se1");
+    var se2 = localStorage.getItem("se2");
+    var se3 = localStorage.getItem("se3");
+    var se4 = localStorage.getItem("se4");
+    var se5 = localStorage.getItem("se5");
     localStorage.setItem="texts",value;
 }
