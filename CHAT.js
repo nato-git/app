c908a10cc46f4456b7bd21741a8e258390772e24
nato-git <s.f.h.o1219@gmail.com>
2025-06-14 01:30:07 +0900

    Update CHAT.js

--- CHAT.js
+++ CHAT.js
@@ -1,43 +1,25 @@
-function SAVE(){
-    const sel1 = document.getElementByld("se1");
-    const sel2 = document.getElementByld("se2");
-    const sel3 = document.getElementByld("se3");
-    const sel4 = document.getElementByld("se4");
-    const sel5 = document.getElementByld("se5");
-    const key1 = selected1;
-    const key2 = selected2;
-    const key3 = selected3;
-    const ket4 = selected4;
-    const key5 = selected5;
-    var names = localStorage.getItem("name")
-    var comments = localStorage.getItem("comment");
-    localStorage.setItem("names",value);
-    localStorage.setItem("comments",value);
-    var texts ="[" + names + "]" + comments + "/n";
-    texts.innerText = texts;
-    localStorage.setItem("texts",value);
-    selectElement.addEventListener("change",() => {
-        localStorage.setItem(key1,sel1.value);
-        localStorage.setItem(key2,sel2.value);
-        localStorage.setItem(key3,sel3.value);
-        localStorage.setItem(key4,sel4.value);
-        localStorage.setItem(key5,sel5.value);
-    });
-    window.addEventListener("DOMContentLoaded", () => {
-        const Savevalue1 = localStorage.getItem("sel1");
-        const Savevalue2 = localStorage.getItem("sel2");
-        const Savevalue3 = localStorage.getItem("sel3");
-        const Savevalue4 = localStorage.getItem("sel4");
-        const Savevalue5 = localStorage.getItem("sel5");
-        if (Savevalue1){
-            sel1.value = Savevalue1;}
-        if (Savevalue2){
-            sel1.value = Savevalue2;}
-        if (Savevalue3){
-            sel1.value = Savevalue3;}
-        if (Savevalue4){
-            sel1.value = Savevalue4;}
-        if (Savevalue5){
-            sel1.value = Savevalue5;}
-    });
+function SAVE() {
+    const sel1 = document.getElementById("se1");
+    const sel2 = document.getElementById("se2");
+    const sel3 = document.getElementById("se3");
+    const sel4 = document.getElementById("se4");
+    const sel5 = document.getElementById("se5");
+
+    const name = document.getElementById("name").value;
+    const comment = document.getElementById("comment").value;
+
+    // 保存
+    localStorage.setItem("sel1", sel1.value);
+    localStorage.setItem("sel2", sel2.value);
+    localStorage.setItem("sel3", sel3.value);
+    localStorage.setItem("sel4", sel4.value);
+    localStorage.setItem("sel5", sel5.value);
+    localStorage.setItem("name", name);
+    localStorage.setItem("comment", comment);
+
+    // 表示用テキストを構築
+    const display = `[${name}] ${comment}\n`;
+    document.getElementById("texts").innerText = display;
+
+    localStorage.setItem("texts", display);
 }
